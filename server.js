a492e161a69a80e2f40b7b0b9c3f732820761946
Daliatarek96 <dalia.tarek.96@hotmail.com>
2025-07-15 19:27:12 +0300

    homeComponent

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require('express'),
+      path = require('path');
 
 
 // server
 const server = express();
 
+// setting
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname,'views'));
+server.use(express.static(path.join(__dirname,'public')));
 
 // middleware
-server.use((req, res)=>{
+server.use((req, res, next)=>{
     console.log(req.url);
-    res.send('welcome back ...');
+    next();
 }); 
-
+server.use((req,res, next)=>{
+    res.render('home');
+});
 // port
 let port = process.env.port;
 server.listen(port|| 8080 ,()=>{
     console.log('I am listening ...');
-});
+});  
